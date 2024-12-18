131ddc2cb341603719610463d3a0ea7cbfcffc00
Ghiorghi777 <goncargeorgij9@gmail.com>
2024-12-18 09:03:37 +0200

    Add files via upload

---
+++ ConsoleApp34/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ConsoleApp34
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Function();
+            Console.ReadKey();
+        }
+        //static void Function()
+        //   {
+        //  int x = 1;
+        //  while(x<= 10)
+        //       {
+        //   Console.WriteLine (x);
+        //   x++;
+        //      }
+        //   }
+        //static void Func()
+        //{
+        //    int x = 1;
+        //    do
+        //    {
+
+        //    }
+        //    while (x <= 10);
+        //static void Function()
+        //{
+        //    for(int x=1;x<=10;x++)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //}
+        //static void Function()
+        //{
+        //    for (int x = 10; x>=; x--)
+        //    {
+        //        Console.WriteLine(x);
+        //    }
+        //}
+    }
+}
+}
