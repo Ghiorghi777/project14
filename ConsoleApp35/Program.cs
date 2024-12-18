ae6dbba9f27adb2376f6c37846c744eb257388f9
Ghiorghi777 <goncargeorgij9@gmail.com>
2024-12-18 09:17:51 +0200

    Add files via upload

---
+++ ConsoleApp35/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp35
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Input("n=");
+            Func(n);
+            Console.ReadKey();
+        }
+        static int Input (string mes)
+        {
+            Console.Write(mes);
+            return int.Parse(Console.ReadLine());
+        }
+        static void Func(int n)
+        {
+            int x = 1;
+            while (x <= n)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            
+        }
+    }
+}
