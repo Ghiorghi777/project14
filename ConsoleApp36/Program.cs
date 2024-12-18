16c0475a22555e167ed5c6f7472ec2d70c285b30
Ghiorghi777 <goncargeorgij9@gmail.com>
2024-12-18 09:22:43 +0200

    Add files via upload

---
+++ ConsoleApp36/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp36
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func();
+            Console.ReadKey();
+        }
+        static void Func()
+        {
+            for (int x = 0; x<=4; x++)
+            {
+                Console.WriteLine("Привет");
+            }
+        }
+    }
+}
